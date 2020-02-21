3ae13f8b3e995de4dce2d4dfb44df4f89ffff4b2
Krzysztof Dyla <57840067+ghostdance386@users.noreply.github.com>
2020-02-21 14:09:04 +0100

    add checkstyle

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,24 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.*;
 
 @Getter
 @Setter
 public class Board {
 
-    public List<Integer> getGameBoard(){
-        List<Integer> gameBoard = new ArrayList<>();
-        for (int i = 0; i <16 ; i++) {
-            gameBoard.add(i);
-        }
-        return gameBoard;
-    }
+  private List<Integer> board = new ArrayList<>();
 
-    public List<Integer> shuffleBoard(List<Integer> board){
-        System.out.println(board.toString());
-        Collections.shuffle(board);
-        System.out.println(board.toString());
-        return board;
+  public List<Integer> getGameBoard() {
+    if (board.isEmpty()) {
+      for (int i = 0; i < 16; i++) {
+        board.add(i);
+      }
     }
+    return board;
+  }
+
+  public List<Integer> shuffleBoard(List<Integer> board) {
+    System.out.println(board.toString());
+    Collections.shuffle(board);
+    System.out.println(board.toString());
+    return board;
+  }
 }
