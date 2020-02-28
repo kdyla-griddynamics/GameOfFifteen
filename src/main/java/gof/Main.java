59a78f5dfb3372378d73cb9b3112e5d002ad28e3
Krzysztof Dyla <57840067+ghostdance386@users.noreply.github.com>
2020-02-28 17:28:40 +0100

    implement manhattan distance and Iterative Deepening A* algorithm

--- src/main/java/gof/Main.java
+++ src/main/java/gof/Main.java
@@ -4,11 +4,22 @@ package gof;
 public class Main {
   public static void main(String[] args) {
 
+    findSolutionAndWriteResults("38k");
+  }
+
+  public static void findSolutionAndWriteResults(String boardSourceFileName) {
     try {
-      Board board = new Board("1");
-      Solver solver = new Solver();
-      Board solvedBoard = solver.solve(board);
-      ResultWriter.writeToFile(solvedBoard, solver);
+      Board board = new Board(boardSourceFileName);
+      Solver solver = new Solver(board);
+      int threshold = board.getManhattanDistance();
+      System.out.println("Board's manhattan distance: " + threshold);
+      while (board.isSolved() < 0) {
+        board = solver.solve(board, 0, threshold);
+        if (board.isSolved() < 0) {
+          threshold = board.getEstimatedMinimumCost();
+        }
+      }
+      ResultWriter.writeToFile(board, solver);
     } catch (BoardNotCompleteException e) {
       System.err.println(e.getMessage());
     }
--- src/main/java/gof/ResultWriter.java
+++ src/main/java/gof/ResultWriter.java
@@ -12,30 +12,26 @@ public class ResultWriter {
     List<Integer> init = solver.getInitial();
     try (FileWriter writer = new FileWriter(result)) {
       writer.write("Initial configuration: \n");
-      for (int i = 0; i < init.size(); i += Board.ROWLENGTH) {
-        writer.write("[ ");
-        for (int j = 0; j < Board.ROWLENGTH; j++) {
-          writer.write(init.get(i + j) + " ");
-        }
-        writer.write("]\n");
-      }
+      writeBoardAsArray(init, writer);
       writer.write("\nNumber of tiles movement: " + board.isSolved() + "\n");
-
-
       for (int i = 0; i < board.getPath().size(); i++) {
         writer.write(board.getPath().get(i) + "\n");
         List<Integer> parent = board.getParents().get(i);
-        for (int j = 0; j < parent.size(); j += Board.ROWLENGTH) {
-          writer.write("[ ");
-          for (int k = 0; k < Board.ROWLENGTH; k++) {
-            writer.write(parent.get(k + j) + " ");
-          }
-          writer.write("]\n");
-        }
+        writeBoardAsArray(parent, writer);
         writer.write("\n");
       }
     } catch (IOException e) {
       e.printStackTrace();
+    }
+  }
+
+  private static void writeBoardAsArray(List<Integer> init, FileWriter writer) throws IOException {
+    for (int i = 0; i < init.size(); i += Board.ROWLENGTH) {
+      writer.write("[ ");
+      for (int j = 0; j < Board.ROWLENGTH; j++) {
+        writer.write(init.get(i + j) + " ");
+      }
+      writer.write("]\n");
     }
   }
 }
